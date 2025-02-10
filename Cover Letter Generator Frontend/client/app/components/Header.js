799cf3b39fc658e8b41ca84d2566fc43314b9f53
Yafet-Siele <167128410+Yafet-Siele@users.noreply.github.com>
2025-02-10 09:53:40 -0700

    Create client
    test

---
+++ Cover Letter Generator Frontend/client/app/components/Header.js
@@ -0,0 +1,43 @@
+"use client"
+import React, { useState } from 'react'
+
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  // Function to toggle the menu
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  return (
+    <div className="flex items-center justify-between p-2 bg-black">
+        {/* Left part: "Cover Craft" */}
+        <div className="flex items-center justify-center py-1.5 px-4 ml-10 border border-white cursor-pointer">
+            <div className="flex space-x-2">
+                <span className="text-white text-2xl font-bold font-['Inter'] tracking-wide">Cover</span>
+                <span className="text-white text-2xl font-bold font-['Inter'] tracking-wide">Craft</span>
+            </div>
+        </div>
+
+        {/* Hamburger Menu Icon (for mobile screens) */}
+        <div className="block md:hidden" onClick={toggleMenu}>
+            <div className="w-6 h-1 bg-white mb-2"></div>
+            <div className="w-6 h-1 bg-white mb-2"></div>
+            <div className="w-6 h-1 bg-white"></div>
+        </div>
+
+        {/* Menu Items (Visible when menu is open on mobile) */}
+        <div className={`flex items-center space-x-6 mr-10 ml-4 ${menuOpen ? 'block' : 'hidden'} md:flex`}>
+            <div className="px-5 py-4 text-white/50 text-[18px] font-normal font-['Inter'] cursor-pointer hover:text-white">
+                New Letter
+            </div>
+            <div className="px-5 py-4 text-white/50 text-[18px] font-normal font-['Inter'] cursor-pointer hover:text-white">
+                Previous Letters
+            </div>
+            <div className="px-4 py-3 bg-white rounded-lg text-black/70 font-bold font-['Inter'] cursor-pointer hover:bg-gray-300">
+                Log out
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ Cover Letter Generator Frontend/client/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DragDropFiles from './components/DragDropFiles'
+import Header from './components/Header'
+
+
+export default function page() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
