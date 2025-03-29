1800d3b55cdf4175de498ade28a140fb67578234
jgaribsin <16392336+jgaribsin@users.noreply.github.com>
2025-03-28 21:32:24 -0600

    fixed server env format

--- server/main.py
+++ server/main.py
@@ -86,4 +86,4 @@ app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host=os.getenv("HOST", "0.0.0.0"), os.getenv("PORT", 8000))
+    uvicorn.run(app, host=os.getenv("HOST", "0.0.0.0"), port=int(os.getenv("PORT", 8000)))
